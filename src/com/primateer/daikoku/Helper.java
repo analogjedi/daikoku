52b8d592ea7f169e1ba58bad3200f63fd9c6b690
vaxr <magnum45@gmx.de>
2013-04-10 15:52:39 +0200

    Date String helper methods

--- src/com/primateer/daikoku/Helper.java
+++ src/com/primateer/daikoku/Helper.java
@@ -2,8 +2,20 @@ package com.primateer.daikoku;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Helper {
+
+	public static final DateFormat DATE_FORMAT = new SimpleDateFormat(
+			"yyyy-MM-dd", Locale.US);
+	public static final Pattern DATE_PATTERN = Pattern
+			.compile("(\\d\\d\\d\\d)-(\\d\\d)-(\\d\\d)");
 
 	public static String loadAsset(String filename) throws IOException {
 		InputStream input = Application.getInstance().getAssets()
@@ -12,5 +24,21 @@ public class Helper {
 		input.read(buffer);
 		input.close();
 		return new String(buffer);
+	}
+
+	public static Date parseDate(String dateString) {
+		Matcher m = DATE_PATTERN.matcher(dateString.trim());
+		if (!m.matches()) {
+			throw new IllegalArgumentException("\"" + dateString
+					+ "\" must be in YYYY-MM-DD format.");
+		}
+		Calendar c = Calendar.getInstance();
+		c.set(Integer.parseInt(m.group(1)), Integer.parseInt(m.group(2)),
+				Integer.parseInt(m.group(3)));
+		return c.getTime();
+	}
+
+	public static String toString(Date date) {
+		return DATE_FORMAT.format(date);
 	}
 }
