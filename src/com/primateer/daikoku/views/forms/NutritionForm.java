803b2fbab9edef7dcb8528b60565305b3f1ced5d
vaxr <magnum45@gmx.de>
2013-05-02 17:22:52 +0200

    moved some row widgets and list adapters into corresponding form classes

--- src/com/primateer/daikoku/views/forms/NutritionForm.java
+++ src/com/primateer/daikoku/views/forms/NutritionForm.java
@@ -7,23 +7,155 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.database.DataSetObserver;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.primateer.daikoku.Application;
 import com.primateer.daikoku.R;
+import com.primateer.daikoku.model.Amount;
 import com.primateer.daikoku.model.Nutrient;
 import com.primateer.daikoku.model.NutrientRegistry;
+import com.primateer.daikoku.model.NutrientSet;
+import com.primateer.daikoku.model.Observer;
+import com.primateer.daikoku.model.SimpleObservable;
+import com.primateer.daikoku.model.Unit;
+import com.primateer.daikoku.model.UnitRegistry;
 import com.primateer.daikoku.model.vos.Nutrition;
-import com.primateer.daikoku.views.lists.NutrientListAdapter;
+import com.primateer.daikoku.views.lists.DataRowListAdapter;
 import com.primateer.daikoku.views.widgets.AmountWidget;
 import com.primateer.daikoku.views.widgets.ReferenceAmountWidget;
 import com.primateer.daikoku.views.widgets.Separator;
+import com.primateer.daikoku.views.widgets.row.DataRowWidget;
 
 public class NutritionForm extends VoForm<Nutrition> {
+
+	private static class NutrientRowWidget extends AmountWidget implements
+			DataRowWidget<Nutrient> {
+
+		private SimpleObservable<DataRowWidget<Nutrient>> widgetObservable = new SimpleObservable<DataRowWidget<Nutrient>>();
+		private TextView label;
+		private ImageButton delButton;
+		private Nutrient.Type type;
+
+		public NutrientRowWidget(Context context, AttributeSet attrs) {
+			super(context, attrs);
+
+			delButton = new ImageButton(context);
+			delButton.setImageResource(Application.ICON_REMOVE);
+			delButton.setBackgroundColor(Color.TRANSPARENT);
+			LinearLayout.LayoutParams deleteLayout = new LayoutParams(0,
+					LayoutParams.WRAP_CONTENT, 0.35f);
+			deleteLayout.gravity = Gravity.CENTER;
+
+			label = new TextView(context);
+			label.setLayoutParams(new LayoutParams(0,
+					LayoutParams.WRAP_CONTENT, 0.7f));
+
+			this.addView(delButton, 0, deleteLayout);
+			this.addView(label, 1);
+
+			this.addObserver(new Observer<Amount>() {
+				@Override
+				public void update(Amount observable) {
+					widgetObservable.notifyObservers(NutrientRowWidget.this);
+				}
+			});
+		}
+
+		public NutrientRowWidget(Context context) {
+			this(context, null);
+		}
+
+		private void setNutrientType(Nutrient.Type type) {
+			this.label.setText(type.toString());
+			this.type = type;
+			List<Unit> units = UnitRegistry.getInstance().getUnitsByType(
+					type.defaultUnit.type);
+			this.setUnits(units);
+			this.selectUnit(type.defaultUnit);
+			widgetObservable.notifyObservers(this);
+		}
+
+		@Override
+		public void storeRowPosition(int pos) {
+			this.setTag(pos);
+		}
+
+		@Override
+		public int restoreRowPosition() {
+			return (Integer) this.getTag();
+		}
+
+		@Override
+		public void setRowData(Nutrient nutrient) {
+			this.setNutrientType(nutrient.type);
+			super.setData(nutrient.amount);
+		}
+
+		@Override
+		public Nutrient getRowData() throws InvalidDataException {
+			return new Nutrient(type, getData());
+
+		}
+
+		@Override
+		public void setOnDeleteRowListener(OnClickListener listener) {
+			delButton.setOnClickListener(listener);
+		}
+
+		@Override
+		public void addRowObserver(Observer<DataRowWidget<Nutrient>> observer) {
+			widgetObservable.addObserver(observer);
+		}
+
+		@Override
+		public void removeRowObserver(Observer<DataRowWidget<Nutrient>> observer) {
+			widgetObservable.removeObserver(observer);
+		}
+	}
+
+	private static class NutrientListAdapter extends
+			DataRowListAdapter<Nutrient> {
+
+		NutrientSet nutrients;
+
+		public NutrientListAdapter() {
+			setNutrients(new NutrientSet());
+		}
+
+		@Override
+		protected DataRowWidget<Nutrient> newWidget(Context context) {
+			return new NutrientRowWidget(context);
+		}
+
+		public void setNutrients(NutrientSet nutrients) {
+			this.nutrients = nutrients;
+			super.data = nutrients;
+			notifyObservers();
+		}
+
+		public Nutrient add(Nutrient.Type type) {
+			Nutrient nutrient = new Nutrient(type, type.getNullAmount());
+			add(nutrient);
+			return nutrient;
+		}
+
+		public NutrientSet getNutrients() {
+			return nutrients;
+		}
+
+		public boolean isTypeOccupied(Nutrient.Type type) {
+			return nutrients.isTypeOccupied(type);
+		}
+	}
 
 	private class ButtonManager extends DataSetObserver {
 		@Override
--- src/com/primateer/daikoku/views/forms/RecipeForm.java
+++ src/com/primateer/daikoku/views/forms/RecipeForm.java
@@ -2,16 +2,21 @@ package com.primateer.daikoku.views.forms;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.primateer.daikoku.Application;
 import com.primateer.daikoku.R;
@@ -21,13 +26,17 @@ import com.primateer.daikoku.model.Component;
 import com.primateer.daikoku.model.Goal;
 import com.primateer.daikoku.model.GoalRegistry;
 import com.primateer.daikoku.model.Observer;
+import com.primateer.daikoku.model.SimpleObservable;
+import com.primateer.daikoku.model.Unit;
+import com.primateer.daikoku.model.UnitRegistry;
 import com.primateer.daikoku.model.vos.Product;
 import com.primateer.daikoku.model.vos.Recipe;
 import com.primateer.daikoku.views.connector.CatalogDialogConnector;
-import com.primateer.daikoku.views.lists.ComponentListAdapter;
 import com.primateer.daikoku.views.lists.DataRowListAdapter;
+import com.primateer.daikoku.views.widgets.AmountWidget;
 import com.primateer.daikoku.views.widgets.LabelWidget;
 import com.primateer.daikoku.views.widgets.NutritionWatchWidget;
+import com.primateer.daikoku.views.widgets.row.DataRowWidget;
 
 public class RecipeForm extends VoForm<Recipe> {
 
@@ -39,6 +48,136 @@ public class RecipeForm extends VoForm<Recipe> {
 	private ImageButton favButton;
 	private boolean favButtonState;
 	private NutritionWatchWidget watchWidget;
+
+	private static class ComponentListAdapter extends
+			DataRowListAdapter<Component> {
+
+		@Override
+		protected DataRowWidget<Component> newWidget(Context context) {
+			return new ComponentRowWidget(context);
+		}
+
+		public void add(Product item) {
+			super.add(new Component(item));
+		}
+
+	}
+
+	private static class ComponentRowWidget extends LinearLayout implements
+			DataRowWidget<Component> {
+
+		private Component component;
+		private ImageButton deleteButton;
+		private TextView selectView;
+		private AmountWidget amountView;
+		private SimpleObservable<DataRowWidget<Component>> observable = new SimpleObservable<DataRowWidget<Component>>();
+
+		public ComponentRowWidget(Context context) {
+			this(context, null);
+		}
+
+		public ComponentRowWidget(Context context, AttributeSet attrs) {
+			super(context, attrs);
+
+			deleteButton = new ImageButton(context);
+			deleteButton.setImageResource(Application.ICON_REMOVE);
+			deleteButton.setBackgroundColor(Color.TRANSPARENT);
+			LinearLayout.LayoutParams deleteLayout = new LayoutParams(0,
+					LayoutParams.WRAP_CONTENT, 0.08f);
+			deleteLayout.gravity = Gravity.CENTER;
+
+			selectView = new TextView(context);
+			selectView.setGravity(Gravity.LEFT | Gravity.CENTER_VERTICAL);
+			LinearLayout.LayoutParams selectLayout = new LayoutParams(0,
+					LayoutParams.MATCH_PARENT, 0.42f);
+			selectLayout.gravity = Gravity.CENTER_VERTICAL;
+			selectView.setPadding(5, 0, 0, 5);
+			selectView.setOnClickListener(new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					CatalogDialogConnector<Product> connector = new CatalogDialogConnector<Product>(
+							Product.class, getContext(),
+							new Observer<Product>() {
+								@Override
+								public void update(Product item) {
+									setRowData(new Component(item));
+								}
+							}, null);
+					connector.getCatalog().loadAll();
+					connector.showDialog();
+				}
+			});
+
+			amountView = new AmountWidget(context);
+			LinearLayout.LayoutParams amountLayout = new LayoutParams(0,
+					LayoutParams.WRAP_CONTENT, 0.6f);
+			amountLayout.gravity = Gravity.CENTER;
+			amountView.addObserver(new Observer<Amount>() {
+				@Override
+				public void update(Amount amount) {
+					component = new Component(component.product, amount);
+					observable.notifyObservers(ComponentRowWidget.this);
+				}
+			});
+
+			this.addView(deleteButton, deleteLayout);
+			this.addView(selectView, selectLayout);
+			this.addView(amountView, amountLayout);
+		}
+
+		@Override
+		public View getView() {
+			return this;
+		}
+
+		@Override
+		public void storeRowPosition(int pos) {
+			this.setTag(pos);
+		}
+
+		@Override
+		public int restoreRowPosition() {
+			return (Integer) this.getTag();
+		}
+
+		@Override
+		public void setRowData(Component data) {
+			this.component = data;
+			selectView.setText(data.product.toString());
+			Set<Unit> permissibleUnits = new HashSet<Unit>();
+			permissibleUnits.add(data.amount.unit);
+			permissibleUnits.addAll(UnitRegistry.getInstance().getUnitsByType(
+					data.amount.unit.type));
+			if (data.product.getUnits() > 0) {
+				permissibleUnits.add(Unit.UNIT_UNITS);
+			}
+			amountView.setUnits(new ArrayList<Unit>(permissibleUnits));
+			amountView.setData(data.amount);
+			observable.notifyObservers(this);
+		}
+
+		@Override
+		public Component getRowData() throws InvalidDataException {
+			return component;
+		}
+
+		@Override
+		public void setOnDeleteRowListener(OnClickListener listener) {
+			deleteButton.setOnClickListener(listener);
+		}
+
+		@Override
+		public void addRowObserver(Observer<DataRowWidget<Component>> observer) {
+			observable.addObserver(observer);
+		}
+
+		@Override
+		public void removeRowObserver(
+				Observer<DataRowWidget<Component>> observer) {
+			observable.removeObserver(observer);
+		}
+
+	}
 
 	public RecipeForm(Context context) {
 		super(context);
