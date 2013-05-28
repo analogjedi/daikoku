1514bc82e5cd3a312d74da67638eb0e52cceb432
vaxr <ricci@primateer.com>
2013-05-28 16:08:25 +0200

    activity description rename

--- src/com/primateer/daikoku/ui/activities/MainActivity.java
+++ src/com/primateer/daikoku/ui/activities/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends FragmentActivity {
 		this.addActivity(MealPlanActivity.class, Application.ICON_CALENDAR,
 				R.string.title_activity_meal_plan);
 		this.addActivity(null, Application.ICON_LIST,
-				R.string.title_activity_supply);
+				R.string.title_activity_shopping);
 		this.addActivity(GoalsActivity.class, Application.ICON_GOALS,
 				R.string.title_activity_goals);
 		this.addActivity(null, Application.ICON_SETTINGS,
