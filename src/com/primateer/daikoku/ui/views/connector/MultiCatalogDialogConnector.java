25c1cc1000936c05ed718237863bd2737205c4d9
vaxr <ricci@primateer.com>
2013-06-03 16:48:37 +0200

    TODO update

--- src/com/primateer/daikoku/ui/views/connector/MultiCatalogDialogConnector.java
+++ src/com/primateer/daikoku/ui/views/connector/MultiCatalogDialogConnector.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.graphics.Color;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.LinearLayout;
